f882452ee6a20a42d34d15d1dd9f994d071f7a44
Egor <oopyatkinoo@mail.ru>
2022-10-23 17:53:57 +0300

    решил задачу 1, семинар #2

--- HW№2/Задача 1/Program.cs
+++ HW№2/Задача 1/Program.cs
@@ -1,2 +1,21 @@
-﻿// See https://aka.ms/new-console-template for more information
-Console.WriteLine("Hello, World!");
+﻿/*
+Задача 10: Напишите программу, которая принимает 
+на вход трёхзначное число и на выходе показывает
+ вторую цифру этого числа.
+*/ 
+Console.WriteLine("Введите трехзначное число:");
+int num1 = Convert.ToInt32(Console.ReadLine());
+SecondNum(num1);
+
+void SecondNum(int number1)
+{
+    if (number1/100>=1 && number1/100<=9)
+    {
+        int result = number1%100/10;
+        Console.WriteLine("Число " + result + " - разряд десятков числа " + num1);
+    }
+    else
+    {
+        Console.WriteLine("Число " + num1 + " не является трехзначным!");
+    }
+}
