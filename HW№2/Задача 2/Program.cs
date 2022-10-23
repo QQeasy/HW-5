3ecafd10b2dcf2561783e8c387524624f0ef49ff
Egor <oopyatkinoo@mail.ru>
2022-10-23 17:59:01 +0300

    решил задачу 2, семинар #2

---
+++ HW№2/Задача 2/Program.cs
@@ -0,0 +1,21 @@
+﻿/*
+Задача 13: Напишите программу, которая 
+выводит третью цифру заданного числа или 
+сообщает, что третьей цифры нет.
+*/ 
+Console.WriteLine("Введите трехзначное число:");
+int num1 = Convert.ToInt32(Console.ReadLine());
+SecondNum(num1);
+
+void SecondNum(int number1)
+{
+    if (number1/100>=1 && number1/100<=9)
+    {
+        int result = number1%10;
+        Console.WriteLine("Число " + result + " - разряд единиц числа " + num1);
+    }
+    else
+    {
+        Console.WriteLine("Число " + num1 + " не является трехзначным!");
+    }
+}
