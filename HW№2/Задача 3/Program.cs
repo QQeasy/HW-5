4c75d8a7e5f12597cf8bbb44701387be960ac66c
Egor <oopyatkinoo@mail.ru>
2022-10-23 20:42:34 +0300

    решил задачу 3, семинар #2

--- HW№2/Задача 3/Program.cs
+++ HW№2/Задача 3/Program.cs
@@ -1,2 +1,29 @@
-﻿// See https://aka.ms/new-console-template for more information
-Console.WriteLine("Hello, World!");
+﻿/*
+Задача 15: Напишите программу, которая принимает на
+вход цифру, обозначающую день недели, и проверяет, 
+является ли этот день выходным.
+*/ 
+int num1 = ReadInt("Введите порядковый номер дня недели:");
+
+Weekend(num1);
+
+// Является ли этот день выходным?
+void Weekend(int number1)
+{
+    string[] arrey1 = { "понедельник", "вторник", "среда", "четверг", "пятница", "суббота", "воскресенье"};
+    if (number1/6>0)
+    {
+        Console.WriteLine("ДА :) " + number1 + "-й день недели - это " + arrey1[number1-1] + ".");
+    }
+    else
+    {
+        Console.WriteLine("НЕТ :( " + number1 + "-й день недели - это " + arrey1[number1-1] + ".");
+    }
+}
+
+// Ввод
+int ReadInt(string massege)
+{
+    Console.Write(massege);
+    return Convert.ToInt32(Console.ReadLine());
+}
