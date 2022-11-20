cce819192d4588acceb301f4b23cbd9ca6652313
Egor <oopyatkinoo@mail.ru>
2022-11-20 14:19:57 +0300

    решил вторую задачу 5 семинар

---
+++ HW№5/Задача 2/Program.cs
@@ -0,0 +1,82 @@
+﻿/*Задача 36: Задайте одномерный массив, заполненный 
+случайными числами. Найдите сумму элементов, стоящих на 
+нечётных позициях.
+[3, 7, 23, 12] -> 19
+[-4, -6, 89, 6] -> 0 
+
+*/
+int size = ReadInt("Введите число элементов массива: ");
+
+int[] arrey = new int[size];
+
+
+ArreyAdd(size);
+Console.WriteLine();
+
+Console.WriteLine("Ваш новый случайный массив: ");
+
+ArreyPrint(arrey);
+Console.WriteLine();
+
+ArreySumOddNum(arrey);
+
+
+Console.WriteLine();
+
+
+
+//Задаёт массив из N чисел
+
+int[] ArreyAdd(int N)
+{
+    for (int i = 0; i < N; i++)
+    {
+        arrey[i] = new Random().Next(-100,100);
+    }
+    return arrey;
+}
+
+
+// Выводит массив
+void ArreyPrint(int[] arrey1)
+{   Console.Write("[ ");
+    for (int i = 0; i < arrey1.Length; i++)
+    {
+        Console.Write(arrey1[i] + " ");
+    }
+    Console.WriteLine("]");
+}
+
+
+
+// Считает к-во четных чисел в массиве
+void ArreySumDouble(int[] arrey2)
+{
+    int SumArrey2 = 0;
+    for (int i = 0; i < arrey2.Length; i++)
+    {
+        if (arrey2[i]%2==0)
+        {
+           SumArrey2 = SumArrey2 + 1;
+        }
+    }        
+    Console.WriteLine(SumArrey2 + " - число четных чисел в массиве ");
+}
+
+// Суммирует все элементы массива, стоящие на нечётных позициях.
+void ArreySumOddNum(int[] arrey2)
+{
+    int Sum1 = 0;
+    for (int i = 1; i < arrey2.Length; i+=2)
+    {
+       Sum1 += arrey2[i];
+    }
+    Console.WriteLine("Сумма элементов, стоящих на нечётных позициях: " + Sum1);
+}
+
+// Ввод
+int ReadInt(string massege)
+{
+    Console.Write(massege);
+    return Convert.ToInt32(Console.ReadLine());
+}
